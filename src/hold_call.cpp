c1a0291c62befbf3bc924af30292af9af16dc696
Salahin <rocky.bgta@gmail.com>
2024-05-30 01:50:41 +0600

    update

--- src/hold_call.cpp
+++ src/hold_call.cpp
@@ -1,7 +1,16 @@
 
 #include "../include/client.h"
+#include <aricpp/arimodel.h>
+#include <nlohmann/json.hpp>
 
 boost::asio::io_context ios;
+using namespace aricpp;
+using json = nlohmann::json;
+
+void Dump(const json& j) {
+    std::cout << j.dump(4) << '\n';  // Pretty print JSON with an indentation of 4 spaces
+}
+
 int main() {
     // Static values for ARI connection
     const std::string host = "192.168.0.180";
@@ -11,10 +20,24 @@ int main() {
     const std::string stasisapp = "my-stasis-app";
 
     aricpp::Client client(ios, host, port, username, password, stasisapp);
-    client.Connect([](boost::system::error_code e) {
-        if (e) std::cerr << "Error connecting: " << e.message() << '\n';
-        else std::cout << "Connected" << '\n';
+    
+    client.Connect([&](boost::system::error_code e) {
+        if (e)
+        {
+            std::cerr << "Asterisk Error connecting: " << e.message() << '\n';
+            return;
+        }
+        std::cout << "Asterisk Connected" << '\n';
+        client.OnEvent("StasisStart", [](const JsonTree& e) {
+            Dump(e); // print the json on the console
+            auto id = Get<std::string>(e, { "channel", "id" });
+            std::cout << "Channel id " << id << " entered stasis application\n";
+            });
         });
+
+    //AriModel channels(client);
+
+   
 
     ios.run();
     return 0;
